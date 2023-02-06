39f7e3315ff75ac426193e12de29eea871efa6e4
shukeel <triggaillest@gmail.com>
2023-02-06 10:06:26 +0300

    setup theme with palette

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,30 @@
+import { Box, IconButton, InputBase, useTheme } from '@mui/material'
+import React, { useContext } from 'react'
+import { ColorModeContext, tokens } from '../../theme';
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1}} placeholder="search" />
+        <IconButton type='button' sx={{p: 1}}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+      <IconButton></IconButton>
+    </Box>
+  )
+}
+
+export default Topbar
